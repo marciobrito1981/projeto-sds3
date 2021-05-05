e1a05c410a662b625f8dcc90a7f84a49aba5e72a
marciobrito1981 <marcio.brito@outlook.com>
2021-05-05 11:45:15 -0300

    Layers

---
+++ backend/src/main/java/com/devsuperiror/dsvendas/services/SellerService.java
@@ -0,0 +1,26 @@
+package com.devsuperiror.dsvendas.services;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperiror.dsvendas.dto.SellerDTO;
+import com.devsuperiror.dsvendas.entities.Seller;
+import com.devsuperiror.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll(){
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+			
+	}
+
+}
